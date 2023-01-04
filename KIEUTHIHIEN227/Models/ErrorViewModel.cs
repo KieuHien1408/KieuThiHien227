7ac964da148c2f1d99db0b8b88fb60cc73a533c2
KieuHien1408 <hienktdk01@gmail.com>
2023-01-04 08:10:40 +0700

    First_commit_KTH227

---
+++ KIEUTHIHIEN227/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KIEUTHIHIEN227.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
